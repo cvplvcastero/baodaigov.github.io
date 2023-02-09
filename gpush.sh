32c7602c495ad5ef1a72782521e573b8bd8e7d1c
baodaigov <khangbietcode1000@gmail.com>
2023-02-09 13:41:12 +0700

    Thêm RSS Feed

---
+++ gpush.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#######################################################################
+#Tập tin này dành để push file lên github một cách nhanh chóng và tiện hơn
+#Author: baodaigov
+#Website: https://baodaigov.github.io
+#######################################################################
+#Chỉnh sửa message của commit
+#git commit --amend -m "Lorem ipsum"
+#git push -f origin master 
+
+git add --all .
+
+read -r -p 'Vui lòng nhập message commit vô đây: ' msg
+
+git commit -m "$msg"
+
+git push -f -u origin master
+
+echo 'Done!'
